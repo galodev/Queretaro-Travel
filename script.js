a987ac69ac1a3982868c59f618f97c69f6d36faa
Galo Vega <contacto.detodounpocomx@gmail.com>
2023-01-24 23:15:40 -0600

    added recent articles section

---
+++ script.js
@@ -0,0 +1,23 @@
+/*
+
+const art4 = document.querySelector(".art4")
+const h4 = document.querySelector(".title-article")
+const price = document.querySelector(".price-article")
+
+
+art4.addEventListener("mouseenter", () => {
+    art4.innerHTML = 
+    `
+    <p><i class="fa-solid fa-location-dot"></i>Jalpan de Serra</p>
+    <p><i class="fa-solid fa-dollar-sign"></i>355</p>
+    <p class="price-article" >El mejor hotel del pais</p>
+    `
+    document.querySelector(".price-article").style.color = white
+})
+
+art4.addEventListener("mouseout", () => {
+    art4.innerHTML = `<h4>Granja el Higuerón</h4>
+                     <h5>Hospedaje sustendable</h5>`
+})
+
+    
